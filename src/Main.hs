709c8cf7567a7a6cec6045048dc4282270f33858
Ketil Malde <ketil@malde.org>
2013-09-03 14:14:16 +0200

    Refactor everything into Main, Metrics, and MPileup
    Ignore-this: ecface83f0bb016b1749c70d54472765
    
    darcs-hash:a741c599069c5387a2d0991647c9ab9de552aa7d

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
 import System.Environment (getArgs)
-import MPileup
+import MPileup (Counts, readPile, showC, showV, major_allele) 
+import Metrics(angle, f_st, pi_k, conf, ci_dist, delta_sigma)
+import Text.Printf
+{-
+import RandomSelect
+import System.Random
+import Control.Parallel
+-}
 
 main :: IO ()
 main = do
@@ -9,5 +16,39 @@ main = do
   ls <-  readPile `fmap` case f of 
     [] -> getContents 
     [fn] -> readFile fn
+  gen_header $ dataids (head ls)
   mapM_ (\x -> do l <- showPile x
                   putStrLn l) ls
+
+-- generate the appropriate header, based on number of input pools
+gen_header :: [String] -> IO ()
+gen_header _ = return ()
+
+dataids :: (String,String,Char,[Counts]) -> [String]
+dataids _ = []
+
+showPile :: (String,String,Char,[Counts]) -> IO String
+showPile (_,_,_,[]) = error "Pileup with no data?"
+showPile (chr,pos,ref,stats@(s1:ss)) = do
+{-  g <- newStdGen
+  let (f,pf) = pval g f_st (s1:ss)
+      (p,pp) = pval g pi_k (s1:ss)
+  pf `par` pp `pseq` -}
+  let cnts = map showC stats
+  return (
+    chr++"\t"++pos++"\t"++[ref]++concat ["\t"++s | s <- map fst cnts]++"\t"++show (sum $ map snd cnts)
+    ++concat ["\t"++conf s1 s | s <- ss]
+    --  ++ conf_all (s1:ss)
+    ++concat [printf "\t%.3f" (angle s1 s) | s <- ss]
+    ++printf "\t%.3f" (f_st (s1:ss))  -- print_pval (f,pf)
+    ++printf "\t%.3f" (pi_k (s1:ss))  -- print_pval (p,pp)
+    ++concat [printf "\t%.2f" (uncurry ci_dist $ major_allele s1 s) | s <- ss]
+    ++concat [printf "\t%.2f" (uncurry (delta_sigma 1) $ major_allele s1 s) | s <- ss]
+    ++concat [printf "\t%.2f" (uncurry (delta_sigma 2) $ major_allele s1 s) | s <- ss]
+    ++"\t"++showV stats)
+
+{-
+print_pval :: (Double, Double) -> String
+print_pval (a,b) = printf "\t%.3f p=%.3f" a b
+-}
+
