ff763d9e5404c5923b68e4b557bd838989a98ba0
Ketil Malde <ketil@malde.org>
2014-09-03 10:32:35 +0200

    VExtr outputs lower case for weak dissent
    Ignore-this: 1b8c1f58510c77c3bce97ebc6db1af82
    
    darcs-hash:ee93d7f7d3c8fa08e6ad03b3dfe4ffdcf87f2815

--- src/VExtr.hs
+++ src/VExtr.hs
@@ -2,7 +2,6 @@ module Main where
 
 import MPileup
 import Count
-import Data.List (foldl')
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 -- TODO: merge as separate mode
@@ -16,25 +15,27 @@ main = do
 makeConsensus :: [MPileRecord] -> String
 makeConsensus = map mp2char
   where mp2char :: MPileRecord -> Char
-        mp2char (MPR ig chr cpos ref cts) = let ss = ptSum cts
-                                                vs = map getV cts
-                                            in selectChar ss
+        mp2char (MPR _ig _chr _cpos _ref cts) = let ss = ptSum cts
+                                                    vs = map getV cts
+                                                in selectChar ss
         selectChar ss = case toList ss of
           [0,0,0,0] -> 'n'
-          [x,0,0,0] -> 'A'
-          [0,x,0,0] -> 'C'
-          [0,0,x,0] -> 'G'
-          [0,0,0,x] -> 'T'
+          [_,0,0,0] -> 'A'
+          [0,_,0,0] -> 'C'
+          [0,0,_,0] -> 'G'
+          [0,0,0,_] -> 'T'
 
-          [x,0,y,0] -> 'R'
-          [0,x,0,y] -> 'Y'
-          [0,x,y,0] -> 'S'
-          [x,0,0,y] -> 'W'
-          [0,0,x,y] -> 'K'
-          [x,y,0,0] -> 'M'
-          [0,x,y,z] -> 'B'
-          [x,0,y,z] -> 'D'
-          [x,y,0,z] -> 'H'
-          [x,y,z,0] -> 'V'
+          [x,0,y,0] -> maybeLower x y 'a' 'g' 'R'
+          [0,x,0,y] -> maybeLower x y 'c' 't' 'Y'
+          [0,x,y,0] -> maybeLower x y 'c' 'g' 'S'
+          [x,0,0,y] -> maybeLower x y 'a' 't' 'W'
+          [0,0,x,y] -> maybeLower x y 'g' 't' 'K'
+          [x,y,0,0] -> maybeLower x y 'a' 'c' 'M'
+
+          [0,_,_,_] -> 'B'
+          [_,0,_,_] -> 'D'
+          [_,_,0,_] -> 'H'
+          [_,_,_,0] -> 'V'
 
           _ -> 'N'
+        maybeLower x y c1 c2 c3 = if x>1 && y >1 then c3 else if x>1 then c1 else c2
