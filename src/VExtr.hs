d314089865417211109c66e251fccc61c3260f17
Ketil Malde <ketil@malde.org>
2014-09-25 13:33:54 +0200

    Lots of development to vextr
    Ignore-this: 39b08645448143d8827fefc2979e82df
    
    darcs-hash:84539a1aec33b019b1a04530bff08c6b47d1c875

--- src/VExtr.hs
+++ src/VExtr.hs
@@ -9,14 +9,16 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import System.Console.CmdArgs
 
 data Options = Opts { infile, outfile :: Maybe FilePath
-                    , xout :: Format
+                    , format :: Format
+                    , fasta :: Bool
                     , mincount :: Int } deriving (Data,Typeable)
 
 opts :: Options
 opts = Opts 
   { infile = Nothing &= args &= typFile
   , outfile = Nothing &= help "output file"
-  , xout = IUPAC &= help "output X, N, or [a/b] instead of IUPAC codes for variable sites"
+  , format = IUPAC &= help "output X, N, or [a/b] instead of IUPAC codes for variable sites"
+  , fasta  = False &= help "output FASTA header"
   , mincount = 1 &= help "ignore counts less than this"
   } &= program "vextr v0.4"
     &= summary "Extract consensus sequence from pooled sequences"
@@ -29,14 +31,32 @@ main = do
   inp <- case infile o of Nothing -> BL.getContents
                           Just f -> BL.readFile f
   let ms = map readPile1 $ BL.lines inp
-  (case outfile o of Nothing -> putStr
-                     Just f -> writeFile f) (makeConsensus (xout o,mincount o) ms)
+      outf = case outfile o of Nothing -> putStr
+                               Just f -> writeFile f
+      gen = if fasta o then makeFasta else makeConsensus
+  outf $ gen (format o,mincount o) ms
+
+
+makeFasta :: (Format,Int) -> [MPileRecord] -> String
+makeFasta fi ms = let
+  header = case ms of
+    (m1:_) -> BL.unpack (chrom m1)++":"++BL.unpack (cpos m1)
+    [] -> ""
+  breaks str = case splitAt 60 str of
+    (rest,"") -> [rest]
+    (this,more) -> this : breaks more
+  in unlines (header:breaks (makeConsensus fi ms))
 
 makeConsensus :: (Format,Int) -> [MPileRecord] -> String
-makeConsensus (iup,mct) = concatMap (fixiup . mp2char)
-  where maybeLower x y c1 c2 c3 = if x>mct && y >mct then c3 else if x>mct then c1 else c2
-        fixiup c | c `elem` "ACGTacgtNn" = [c]
-                 | otherwise             = case iup of
+makeConsensus (iup,mct) = concatMap (fixiup iup . selectChar mct . ptSum . counts)
+  -- todo: include variants
+
+-- this doesn't work so well with high coverage/many libraries
+
+-- | Optionally change from IUPAC code to X or regex
+fixiup :: Format -> Char -> String
+fixiup iup c | c `elem` "ACGTacgtNn" = [c]
+             | otherwise             = case iup of
           Xs -> "X"
           IUPAC -> [c]
           Regex -> case c of
@@ -51,23 +71,22 @@ makeConsensus (iup,mct) = concatMap (fixiup . mp2char)
             'H' -> "[A/C/T]"
             'V' -> "[A/C/G]"
             x   -> [x]
-        mp2char :: MPileRecord -> Char
-        mp2char (MPR _ig _chr _cpos _ref cts) = let ss = ptSum cts
-                                                    -- vs = map getV cts
-                                                in selectChar ss
-        selectChar ss = case toList ss of
+
+-- | Convert allele counts into IUPAC character
+selectChar :: Int -> Counts -> Char
+selectChar mct ss = case toList ss of
           [0,0,0,0] -> 'n'
           [_,0,0,0] -> 'A'
           [0,_,0,0] -> 'C'
           [0,0,_,0] -> 'G'
           [0,0,0,_] -> 'T'
 
-          [x,0,y,0] -> maybeLower x y 'a' 'g' 'R'
-          [0,x,0,y] -> maybeLower x y 'c' 't' 'Y'
-          [0,x,y,0] -> maybeLower x y 'c' 'g' 'S'
-          [x,0,0,y] -> maybeLower x y 'a' 't' 'W'
-          [0,0,x,y] -> maybeLower x y 'g' 't' 'K'
-          [x,y,0,0] -> maybeLower x y 'a' 'c' 'M'
+          [x,0,y,0] -> maybeWild x y 'a' 'g' 'R'
+          [0,x,0,y] -> maybeWild x y 'c' 't' 'Y'
+          [0,x,y,0] -> maybeWild x y 'c' 'g' 'S'
+          [x,0,0,y] -> maybeWild x y 'a' 't' 'W'
+          [0,0,x,y] -> maybeWild x y 'g' 't' 'K'
+          [x,y,0,0] -> maybeWild x y 'a' 'c' 'M'
 
           [0,_,_,_] -> 'B'
           [_,0,_,_] -> 'D'
@@ -75,3 +94,5 @@ makeConsensus (iup,mct) = concatMap (fixiup . mp2char)
           [_,_,_,0] -> 'V'
 
           _ -> 'N'
+  where maybeWild x y c1 c2 c3 = if x>mct && y >mct then c3 else if x>mct then c1 else c2
+        
