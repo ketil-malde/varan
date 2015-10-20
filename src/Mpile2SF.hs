7fe6aeea3949930f97ba141d9528c02dd6af7bad
Ketil Malde <ketil@malde.org>
2015-10-20 12:56:24 +0200

    ignore missing/ambigous ref
    Ignore-this: b7d1ce1120a32f8d6e74470823f4e58e
    
    darcs-hash:149414cfd5754926ee0155238023d6819f5e4705

--- src/Mpile2SF.hs
+++ src/Mpile2SF.hs
@@ -20,8 +20,10 @@ main = do
   mapM_ B.putStrLn . catMaybes $ map mp2sf mps
 
 mp2sf :: MPileRecord -> Maybe B.ByteString
-mp2sf mpr = if ignore mpr then Nothing 
-            else Just (B.append (cpos mpr) (B.pack (printf "\t%d\t%d\t1"  refs tot)))
+mp2sf mpr
+  | ignore mpr = Nothing
+  | not (refnuc mpr `elem` "ACGTacgt") = Nothing
+  | otherwise  = Just (B.append (cpos mpr) (B.pack (printf "\t%d\t%d\t1"  refs tot)))
   where alleles = toList $ ptSum $ counts mpr :: [Int]
         tot  = sum alleles :: Int
         refs = alleles!!(case refnuc mpr of {'A' -> 0; 'C' -> 1; 'G' -> 2; 'T' -> 3; 'a' -> 0; 'c' -> 1; 'g' -> 2; 't' -> 3}) :: Int
