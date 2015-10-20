2bc5ba9dc1ff7d781f2994cde40039650d72b315
Ketil Malde <ketil@malde.org>
2015-10-20 12:47:32 +0200

    use maybe to avoid empty lines
    Ignore-this: 543ce0feb447fdd90c4415f001a686ec
    
    darcs-hash:a822583710c10f01f540af01de0172517d7fe89c

--- src/Mpile2SF.hs
+++ src/Mpile2SF.hs
@@ -7,20 +7,21 @@
 
 module Main where
 
-import Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as B
 import Text.Printf
+import Data.Maybe
 
 import MPileup
 import Count
 
 main :: IO ()
 main = do
-  mps <- Prelude.map readPile1 `fmap` B.lines `fmap` B.getContents
-  mapM_ (B.putStrLn . mp2sf) mps
+  mps <- map readPile1 `fmap` B.lines `fmap` B.getContents
+  mapM_ B.putStrLn . catMaybes $ map mp2sf mps
 
-mp2sf :: MPileRecord -> ByteString
-mp2sf mpr = if ignore mpr then B.empty
-            else B.append (cpos mpr) (B.pack (printf "\t%d\t%d\t1"  refs tot))
+mp2sf :: MPileRecord -> Maybe B.ByteString
+mp2sf mpr = if ignore mpr then Nothing 
+            else Just (B.append (cpos mpr) (B.pack (printf "\t%d\t%d\t1"  refs tot)))
   where alleles = toList $ ptSum $ counts mpr :: [Int]
         tot  = sum alleles :: Int
         refs = alleles!!(case refnuc mpr of {'A' -> 0; 'C' -> 1; 'G' -> 2; 'T' -> 3; 'a' -> 0; 'c' -> 1; 'g' -> 2; 't' -> 3}) :: Int
