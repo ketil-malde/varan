38748c7a2f497a2b939847bf48d2dd324f35f408
Ketil Malde <ketil@malde.org>
2015-10-20 12:34:12 +0200

    Add Mpile2SF executable
    Ignore-this: bb4db42cdcd1d06dc8d4ebb6e3d6d994
    
    darcs-hash:0c1ea41e4472c01a5b23733fd9e8cbd710fe3c7f

---
+++ src/Mpile2SF.hs
@@ -0,0 +1,26 @@
+{-
+  Convert MPile to SweepFinder input, i.e.
+  <position> <x> <n> <folded>
+
+  I guess all will have to be folded?  Or use ref as ancestor?  How does it matter?
+-}
+
+module Main where
+
+import Data.ByteString.Lazy.Char8 as B
+import Text.Printf
+
+import MPileup
+import Count
+
+main :: IO ()
+main = do
+  mps <- Prelude.map readPile1 `fmap` B.lines `fmap` B.getContents
+  mapM_ (B.putStrLn . mp2sf) mps
+
+mp2sf :: MPileRecord -> ByteString
+mp2sf mpr = if ignore mpr then B.empty
+            else B.append (cpos mpr) (B.pack (printf "\t%d\td\t1"  refs tot))
+  where alleles = toList $ ptSum $ counts mpr :: [Int]
+        tot  = sum alleles :: Int
+        refs = alleles!!(case refnuc mpr of {'A' -> 0; 'C' -> 1; 'G' -> 2; 'T' -> 3; 'a' -> 0; 'c' -> 1; 'g' -> 2; 't' -> 3}) :: Int
